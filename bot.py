593b4ffdc04d032e94483a959cd84a1485e9b3b8
Matt Sehgal <matt1913@brandeis.edu>
2020-11-10 13:39:43 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 """
 
 @author: Matt Sehgal
-@purpose: main file for slack bot
+@purpose: base for Bot
 """
 
 import slack_sdk
@@ -22,16 +22,18 @@ token = os.environ['SLACK_DEV_TOKEN']
 client = slack_sdk.WebClient(token=token)
 bot_id = client.api_call('auth.test')['user_id']
 event_handler = slack_event_handler.EventHandler(client)
-        
-
 
 @slack_event_adapter.on('reaction_added')
 @slack_event_adapter.on('message')
 def event_listener(payload):
+    '''
+    Passes event payload onto event handler,
+    returns 200 OK to server. This framework is 
+    sadly necessary as Slack will resend messages if the 
+    response code is still processing after 3 seconds.
+    '''
     event_handler.handle(payload)
     return Response(status=200)
-
-
 
 if __name__ == "__main__":
     app.run(debug=False)
